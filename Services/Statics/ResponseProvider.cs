9ba95238dff526cdaf06e7097b97181e1dd3ed43
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2023-10-08 12:44:23 -0600

    chore: :sparkels: add static services

---
+++ Services/Statics/ResponseProvider.cs
@@ -0,0 +1,27 @@
+﻿namespace ESFE.Chatbot.Statics;
+
+public class ResponseProvider
+{
+
+  public static class Res
+  {
+    public static IReturn<T> Provider<T>(T data, string message, bool success)
+    {
+      return new Return<T> { Data = data, Message = message, Success = success };
+    }
+  }
+
+  public interface IReturn<T>
+  {
+    T Data { get; set; }
+    string Message { get; set; }
+    bool Success { get; set; }
+  }
+
+  public class Return<T> : IReturn<T>
+  {
+    public required T Data { get; set; }
+    public required string Message { get; set; }
+    public bool Success { get; set; }
+  }
+}
---
+++ Services/Statics/UtilsService.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ESFE.Chatbot.Statics;
+
+public class UtilsService
+{
+  private static Random random = new Random();
+
+  public static string ConvertSHA256(string text)
+  {
+    string hash = string.Empty;
+    using (SHA256 sha256 = SHA256.Create())
+    {
+      //obtener el hash recibido
+      byte[] hashValue = sha256.ComputeHash(Encoding.UTF8.GetBytes(text));
+
+      // convertir array de bytes en cadena de texto
+      foreach (byte b in hashValue)
+      {
+        hash += $"{b:X2}";
+      }
+    }
+    return hash;
+  }
+
+  public static string RandomCode()
+  {
+    const int length = 6;
+    const string chars = "0123456789";
+    char[] code = new char[length];
+
+    for (int i = 0; i < length; i++)
+    {
+      code[i] = chars[random.Next(chars.Length)];
+    }
+
+    return new string(code);
+  }
+}
