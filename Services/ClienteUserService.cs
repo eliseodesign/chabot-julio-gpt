8690a5cac1b42143816c2af2e950e365f5f65095
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2023-10-08 18:09:11 -0600

    chore: :sparkles: partial clientUser service

---
+++ Services/ClienteUserService.cs
@@ -0,0 +1,78 @@
+﻿using ESFE.Chatbot.Models;
+using ESFE.Chatbot.Repositories;
+using ESFE.Chatbot.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ESFE.Chatbot.Services;
+
+public class ClienteUserService : IClienteUserService
+{
+
+    private readonly IGenericRepository<ClientUser> _userRepo;
+    public ClienteUserService(IGenericRepository<ClientUser> userRepo)
+    {
+        _userRepo = userRepo;
+    }
+
+    public async Task<bool> Create(ClientUser model) => await _userRepo.Create(model);
+
+    public async Task<bool> Update(ClientUser model) => await _userRepo.Update(model);
+    public async Task<bool> Delete(int id) => await _userRepo.Delete(id);
+
+    public Task<IQueryable<ClientUser>> GetAll() => _userRepo.GetAll();
+    public async Task<bool> ConfirmAccount(string token, int userId)
+    {
+        try
+        {
+            var users = await _userRepo.GetAll();
+            ClientUser existingClientUser = await users.FirstAsync(a => a.Id == userId && a.Token == token);
+            existingClientUser.ConfirmAccount = true;
+
+            await _userRepo.Update(existingClientUser);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public async Task<ClientUser?> GetByEmail(string email)
+    {
+        try
+        {
+            var users = await _userRepo.GetAll();
+            var result = await users.FirstAsync(a => a.Email == email);
+            return result;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public Task<ClientUser?> GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> RestartAccount(bool restart, string password, string token)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    public async Task<ClientUser?> Validate(string email, string password)
+    {
+        try
+        {
+            var users = await _userRepo.GetAll();
+            var result = await users.FirstAsync(a => a.Email == email && a.Password == password);
+            return result;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
---
+++ Services/Interfaces/IClienteUserService.cs
@@ -0,0 +1,16 @@
+﻿using ESFE.Chatbot.Models;
+
+namespace ESFE.Chatbot.Services.Interfaces;
+
+public interface IClienteUserService
+{
+  Task<bool> Create(ClientUser model);
+  Task<bool> Update(ClientUser model);
+  Task<bool> Delete(int id);
+  Task<IQueryable<ClientUser>> GetAll();
+  Task<ClientUser?> GetById(int id);
+  Task<ClientUser?> Validate(string email, string password);
+  Task<ClientUser?> GetByEmail(string email);
+  Task<bool> RestartAccount(bool restart, string password, string token);
+  Task<bool> ConfirmAccount(string token, int userID);
+}
