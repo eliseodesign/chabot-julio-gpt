c5710ce2ffb00b78e9f3e38bd5305053f02cd784
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2023-10-08 20:13:12 -0600

    chore: :sparkles: dependencie inyection

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
+using ESFE.Chatbot;
+using ESFE.Chatbot.Models;
+using ESFE.Chatbot.Repositories;
+using ESFE.Chatbot.Services;
+using ESFE.Chatbot.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ChatBotDbContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ChatBotDbContext"));
+});
+
+builder.Services.AddScoped<IGenericRepository<ClientUser>, ClientUserRepository>();
+builder.Services.AddScoped<IGenericRepository<FeedBack>, FeedBackRepository>();
+builder.Services.AddScoped<IGenericRepository<TypeUser>, TypeUserRepository>();
+
+builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddScoped<IClienteUserService, ClienteUserService>();
+
 
 var app = builder.Build();
 
