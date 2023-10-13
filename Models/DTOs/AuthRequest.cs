44b1ce90665e9d114b0b5dd56bebfab2b8278b42
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2023-10-13 15:00:02 -0600

    chore: :sparkles: add model and interface of auth

---
+++ Models/DTOs/AuthRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace ESFE.Chatbot.Models.DTOs;
+
+public class AuthRequest
+{
+    public required string UserName { get; set; }
+    public required string Password { get; set; }
+}
---
+++ Models/DTOs/AuthResponse.cs
@@ -0,0 +1,10 @@
+﻿using ESFE.Chatbot.Models;
+namespace ESFE.Chatbot.Models.DTOs;
+
+public class AuthResponse
+{
+    public required string Token { get; set; }
+    public bool Result { get; set; }
+    public required string Message { get; set; }
+    public ClientUser? User {get;set;}
+}
