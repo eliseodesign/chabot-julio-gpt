4f951ce188d6d91c8b30c3eea1c1368f76e99058
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2023-12-03 21:02:27 -0600

    chore: parche temporal

--- Middlewares/RequestLimitUser.cs
+++ Middlewares/RequestLimitUser.cs
@@ -29,7 +29,7 @@ namespace ESFE.Chatbot
         public async Task Invoke(HttpContext context)
         {
             // Verifica si la solicitud está dirigida a un endpoint específico
-            if (context.Request.Path == "/api/chat/user")
+            if (context.Request.Path == "/api/chat/none")
             {
                 // Obtiene el token JWT del encabezado de la solicitud
                 var jwtToken = context.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
